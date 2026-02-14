5f7f4013e59c3cccbeb49a2df43951b055b1c789
sakethkasyapgudipati-creator <sakethkasyap.gudipati@nxtwave.co.in>
2026-02-14 15:48:42 +0530

    Initial commit: MERN chat app setup

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();	
+
+
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(process.env.MONGO_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
